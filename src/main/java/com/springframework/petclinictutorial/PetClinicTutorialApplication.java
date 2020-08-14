c3cd1c3f9fb1e3e8e924eec4d4510989e1341ddd
sousajo <josesousa@rentalcars.com>
2020-08-14 23:21:49 +0100

    "initial commit"

---
+++ src/main/java/com/springframework/petclinictutorial/PetClinicTutorialApplication.java
@@ -0,0 +1,14 @@
+package com.springframework.petclinictutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicTutorialApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PetClinicTutorialApplication.class, args);
+    }
+
+}
