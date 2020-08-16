228a0739cc8024dc14458f204deea7a351c501dc
sousajo <josesousa@rentalcars.com>
2020-08-16 03:59:55 +0100

    changes in CI CD pipeline

---
+++ src/main/java/com/springframework/petclinictutorial/controllers/TestController.java
@@ -0,0 +1,25 @@
+package com.springframework.petclinictutorial.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by sousaJ on 16/08/2020
+ * in package - com.springframework.petclinictutorial.controllers
+ **/
+
+@Controller
+public class TestController {
+
+    @Value("${welcome.message}")
+    private String message;
+
+    @GetMapping("/")
+    public String getString(Model model){
+        model.addAttribute("message", message);
+        return "index";
+    }
+}
