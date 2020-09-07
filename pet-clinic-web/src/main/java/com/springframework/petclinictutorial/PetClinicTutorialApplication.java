fadbcbfddef370b8b1d283514b81b3b7435f3803
sousajo <josesousa@rentalcars.com>
2020-09-08 00:00:15 +0100

    remove test on env var

--- pet-clinic-web/src/main/java/com/springframework/petclinictutorial/PetClinicTutorialApplication.java
+++ pet-clinic-web/src/main/java/com/springframework/petclinictutorial/PetClinicTutorialApplication.java
@@ -1,21 +1,13 @@
 package com.springframework.petclinictutorial;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-@Slf4j
 public class PetClinicTutorialApplication {
 
     public static void main(String[] args) {
-
-        ApplicationContext ctx = SpringApplication.run(PetClinicTutorialApplication.class, args);
-
-        String testEnv = (String) ctx.getBean("testAwsEnv");
-
-        log.info(testEnv);
+        SpringApplication.run(PetClinicTutorialApplication.class, args);
     }
 
 }
