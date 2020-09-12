130fa0df76474e25310a2205a5ac34835ee63076
sousajo <josesousa@rentalcars.com>
2020-09-12 17:26:15 +0100

    Created Visit, Pet and added properties to pet types  closes #38

--- pet-clinic-data/src/main/java/com/springframework/petclinictutorial/model/PetType.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinictutorial/model/PetType.java
@@ -1,11 +1,31 @@
 package com.springframework.petclinictutorial.model;
 
+import java.time.LocalDate;
+
 /**
  * Created by sousaJ on 24/08/2020
  * in package - com.springframework.petclinictutorial.model
  **/
 public class PetType extends BaseEntity {
     private String name;
+    private Owner owner;
+    private LocalDate birthDate;
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
 
     public String getName() {
         return name;
