eb2ea20ce8712af845c19fb854f4b5784f39bc87
sousajo <josesousa@rentalcars.com>
2020-09-12 17:26:15 +0100

    Created Visit, Pet and added properties to pet types  #38

--- pet-clinic-data/src/main/java/com/springframework/petclinictutorial/model/Owner.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinictutorial/model/Owner.java
@@ -1,11 +1,20 @@
 package com.springframework.petclinictutorial.model;
 
+import java.util.Set;
+
 /**
  * Created by sousaJ on 24/08/2020
  * in package - com.springframework.petclinictutorial.model
  **/
 
 public class Owner extends Person {
+    private Set<Pet> pets;
 
+    public Set<Pet> getPets() {
+        return pets;
+    }
 
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
