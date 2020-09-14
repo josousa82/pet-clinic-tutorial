2fee467964f19fee961a692fb4a3eb6f53f415ef
sousajo <josesousa@rentalcars.com>
2020-09-14 18:43:32 +0100

    Added specialty service interface and implementation closes #43

---
+++ pet-clinic-data/src/main/java/com/springframework/petclinictutorial/services/SpecialtiesService.java
@@ -0,0 +1,10 @@
+package com.springframework.petclinictutorial.services;
+
+import com.springframework.petclinictutorial.model.Specialty;
+
+/**
+ * Created by sousaJ on 14/09/2020
+ * in package - com.springframework.petclinictutorial.services
+ **/
+public interface SpecialtiesService extends CrudService<Specialty, Long>{
+}
---
+++ pet-clinic-data/src/main/java/com/springframework/petclinictutorial/services/map/SpecialtyMapService.java
@@ -0,0 +1,39 @@
+package com.springframework.petclinictutorial.services.map;
+
+import com.springframework.petclinictutorial.model.Specialty;
+import com.springframework.petclinictutorial.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Created by sousaJ on 14/09/2020
+ * in package - com.springframework.petclinictutorial.services.map
+ **/
+@Service
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
