7b0e7e37d0b9857334a1e02a244a6c5de0815123
sousajo <josesousa@rentalcars.com>
2020-10-05 22:18:18 +0100

    Created spring data jpa service for PetType. Closes #54

--- pet-clinic-data/src/main/java/com/springframework/petclinictutorial/model/repositories/PetTypeRepository.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinictutorial/model/repositories/PetTypeRepository.java
@@ -2,10 +2,12 @@ package com.springframework.petclinictutorial.model.repositories;
 
 import com.springframework.petclinictutorial.model.PetType;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by sousaJ on 05/10/2020
  * in package - com.springframework.petclinictutorial.model.repositories
  **/
+@Repository
 public interface PetTypeRepository extends CrudRepository<PetType, Long> {
 }
---
+++ pet-clinic-data/src/main/java/com/springframework/petclinictutorial/services/springdatajpa/PetTypeSDJpaService.java
@@ -0,0 +1,52 @@
+package com.springframework.petclinictutorial.services.springdatajpa;
+
+import com.springframework.petclinictutorial.model.PetType;
+import com.springframework.petclinictutorial.model.repositories.PetTypeRepository;
+import com.springframework.petclinictutorial.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by sousaJ on 05/10/2020
+ * in package - com.springframework.petclinictutorial.services.springdatajpa
+ **/
+@Service
+@Profile("springdatajpa")
+public class PetTypeSDJpaService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeSDJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return petTypeRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return petTypeRepository.save(petType);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        petTypeRepository.delete(petType);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petTypeRepository.deleteById(id);
+    }
+}
