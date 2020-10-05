6033c2fd2369f945e3ba4ecb0bbe879c20987ed4
sousajo <josesousa@rentalcars.com>
2020-10-05 22:11:34 +0100

    Created spring data jpa service for Vet. Closes #53

--- pet-clinic-data/src/main/java/com/springframework/petclinictutorial/model/repositories/VetRepository.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinictutorial/model/repositories/VetRepository.java
@@ -2,10 +2,12 @@ package com.springframework.petclinictutorial.model.repositories;
 
 import com.springframework.petclinictutorial.model.Vet;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by sousaJ on 05/10/2020
  * in package - com.springframework.petclinictutorial.model.repositories
  **/
+@Repository
 public interface VetRepository extends CrudRepository<Vet, Long> {
 }
---
+++ pet-clinic-data/src/main/java/com/springframework/petclinictutorial/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,52 @@
+package com.springframework.petclinictutorial.services.springdatajpa;
+
+import com.springframework.petclinictutorial.model.Vet;
+import com.springframework.petclinictutorial.model.repositories.VetRepository;
+import com.springframework.petclinictutorial.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by sousaJ on 05/10/2020
+ * in package - com.springframework.petclinictutorial.services.springdatajpa
+ **/
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(new HashSet<>()::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return vetRepository.save(vet);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        vetRepository.delete(vet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}
