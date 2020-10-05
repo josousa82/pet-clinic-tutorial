89cce510f677622949678a42415a5c212a0bd1bb
sousajo <josesousa@rentalcars.com>
2020-10-05 23:14:38 +0100

    Implemented VisitSDJpaService, visit service for JPA. Closes #63

--- pet-clinic-data/src/main/java/com/springframework/petclinictutorial/model/repositories/VisitRepository.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinictutorial/model/repositories/VisitRepository.java
@@ -2,10 +2,12 @@ package com.springframework.petclinictutorial.model.repositories;
 
 import com.springframework.petclinictutorial.model.Visit;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by sousaJ on 05/10/2020
  * in package - com.springframework.petclinictutorial.model.repositories
  **/
+@Repository
 public interface VisitRepository extends CrudRepository<Visit, Long> {
 }
---
+++ pet-clinic-data/src/main/java/com/springframework/petclinictutorial/services/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,52 @@
+package com.springframework.petclinictutorial.services.springdatajpa;
+
+import com.springframework.petclinictutorial.model.Visit;
+import com.springframework.petclinictutorial.model.repositories.VisitRepository;
+import com.springframework.petclinictutorial.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by sousaJ on 05/10/2020
+ * in package - com.springframework.petclinictutorial.services.springdatajpa
+ **/
+@Service
+@Profile("springdatajpa")
+public class VisitSDJpaService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitSDJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return visitRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        return visitRepository.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        visitRepository.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        visitRepository.deleteById(id);
+    }
+}
