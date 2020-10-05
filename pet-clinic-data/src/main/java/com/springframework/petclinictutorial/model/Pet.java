70a1e516dd77e9d6434de2766ef04dea2cd1ac99
sousajo <josesousa@rentalcars.com>
2020-10-05 19:51:07 +0100

    Converted visit and related entities to JPA entities. Closes #50

--- pet-clinic-data/src/main/java/com/springframework/petclinictutorial/model/Pet.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinictutorial/model/Pet.java
@@ -2,6 +2,8 @@ package com.springframework.petclinictutorial.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by sousaJ on 24/08/2020
@@ -25,6 +27,9 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
--- pet-clinic-data/src/main/java/com/springframework/petclinictutorial/model/Visit.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinictutorial/model/Visit.java
@@ -1,15 +1,25 @@
 package com.springframework.petclinictutorial.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * Created by sousaJ on 12/09/2020
  * in package - com.springframework.petclinictutorial.model
  **/
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
